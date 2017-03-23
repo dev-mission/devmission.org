4eaa136d20fafdc1c586b5303f05d6600c8e4a9f
SalasC2 <christiansalas96@gmail.com>
2017-03-22 19:15:35 -0700

    site files automatically added after recompiling server

--- _site/js/main.js
+++ _site/js/main.js
@@ -7,4 +7,39 @@ $(document).ready(function(){
 		    zoom: 8
 		  });
 		}
-}); 
+	submitForm();
+});
+
+function submitForm() {
+
+	$('#contact-form-submit').click(function(e) {
+    e.preventDefault();
+    var contactName = $('#contact-name').val();
+    var contactEmail = $('#contact-email').val();
+    var contactNumber = $('#contact-number').val();
+    var contactPurpose = $('#contact-purpose').val();
+    // data validation code here
+    var url = "https://docs.google.com/forms/d/e/1FAIpQLSc4n_ZafSq1_QVjzJSxThtQjOUpD-YjlLcRW2eFhO_jSNaPUw/formResponse";
+    var data = {
+        'entry.564562331': contactName,
+        'entry.253022276': contactEmail,
+        'entry.587420216': contactPurpose,
+    };
+    $.ajax({
+            type: "POST",
+            url: url,
+            dataType: "json",
+            data: data,
+            statusCode: {
+                    0: function() {
+                            console.log("unknown");
+                            window.location.href = "index";
+                    },
+                    200: function() {
+                            console.log("success");
+                            window.location.href = "success";
+                    }
+            }
+    });
+});
+}
