c9bd06270c05c50ffcb7a0673b83e6f66d721f6d
SalasC2 <christiansalas96@gmail.com>
2017-03-28 01:49:16 -0700

    Compiled pages

--- _site/js/main.js
+++ _site/js/main.js
@@ -55,22 +55,44 @@ function submitParticipateForm() {
 
 	$('#participate-form-submit').click(function(e) {
     e.preventDefault();
-    var contactName = $('#contact-name').val();
-		var contactTitle = $('#contact-title').val();
-    var contactEmail = $('#contact-email').val();
-		var contactAbout = $('#contact-about').val();
-    var contactPurpose = $('#contact-purpose').val();
-		var contactAdditional = $('#contact-additional').val();
-    // data validation code here
-    var url = "";
-    var data = {
-        'entry.2005620554': contactName,
-				'entry.2120814685': contactTitle,
-        'entry.99450920': contactEmail,
-				'entry.777197893': contactAbout,
-        'entry.1576241862': contactPurpose,
-				'entry.371473633"': contactAdditional
+    var contactFirstName = $('#contact-first-name').val();
+		var contactMiddleName = $('#contact-middle-name')
+		var contactLastName = $('#contact-last-name').val();
+    var contactAddress = $('#contact-address').val();
+		var contactCity = $('#contact-city').val();
+    var contactZipCode = $('#contact-zip-code').val();
+		var contactDateOfBirth = $('#contact-date-of-birth').val();
+		var contactHomeNumber= $('#contact-home-number').val();
+		var contactMobileNumber = $('#contact-mobile-number').val();
+		var contactEmail = $('#contact-email').val();
+		var contactAge = $('#contact-gender').val();
+		var contactGender = $('#contact-date-of-birth').val();
+		var contactIdentification = $('#contact-identification').val();
+		var programCommitment = $('#commmitment').val();
+		var joinReasoning = $('#join-reasoning').val();
+		var gradGoals = $('#grad-goals').val();
+		var programSession = $('#session').val();
 
+    // data validation code here
+    var url = "https://docs.google.com/forms/d/e/1FAIpQLSdKsQPVWx5rwN-O6GN-4JmqXnDld3idHVKNOtjkL9v-cTbvVw/formResponse";
+    var data = {
+			'entry.164084543': contactFirstName,
+			'entry.164084543': contactMiddleName,
+			'entry.206814970': contactLastName,
+			'entry.1358523807': contactAddress,
+			'entry.952400269': contactCity,
+			'entry.511556928': contactZipCode,
+			'entry.988194661': contactDateOfBirth,
+			'entry.2088293968': contactHomeNumber,
+			'entry.2088293968': contactMobileNumber,
+			'entry.285442385': contactEmail,
+			'entry.84273717': contactAge,
+			'entry.966779481': contactGender,
+			'entry.252558192': contactIdentification,
+			'entry.311765769': programCommitment,
+			'entry.2081252789': joinReasoning,
+			'entry.1717696693': gradGoals,
+			'entry.1221357470': programSession
     };
     $.ajax({
             type: "POST",
